b59e14abe7769903acb4d19a04a9ff780cd74c0c
Ivankataasenkata <Ivanasen98@abv.bg>
2025-01-18 03:44:19 -0800

    The file ServerUsers.js

---
+++ ServerCode.js
@@ -0,0 +1,13 @@
+
+//The function takes three params
+//The main function
+function main(){
+    function calc(a, b, c , d){
+        return a + b + c + d;
+    }
+
+    const result = calc(10,3);
+    console.log("The sum is: ", result);
+}
+
+main();
